ce96f501247c52aa10b2747ace71960c50b5acce
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2015-05-21 17:20:26 +0200

    Add test case with Rosenbrock function

---
+++ bolero/optimizer/test/test_rosenbrock.py
@@ -0,0 +1,24 @@
+"""Test for unimodal function optimization."""
+import numpy as np
+from nose.tools import assert_less
+from bolero.optimizer import CMAESOptimizer
+from bolero.environment.objective_functions import Rosenbrock
+
+
+def eval_loop(x, opt, n_dims, n_evals=1000):
+    objective = Rosenbrock(0, n_dims)
+    results = np.empty(n_evals)
+    for i in xrange(n_evals):
+        opt.get_next_parameters(x)
+        results[i] = objective.feedback(x)
+        opt.set_evaluation_feedback(results[i])
+    return results - objective.f_opt
+
+
+def test_cmaes(n_dims=2):
+    x = np.zeros(n_dims)
+    opt = CMAESOptimizer(x, random_state=0, log_to_stdout=False)
+    opt.init(n_dims)
+    r = eval_loop(x, opt, n_dims)
+    assert_less(-1e-5, r.max())
+    return r
