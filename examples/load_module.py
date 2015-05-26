3c4e6d1e599495662a6adf9d03a7c05c5469d56a
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2015-05-26 15:08:23 +0200

    Add example for configuration dict

---
+++ examples/load_module.py
@@ -0,0 +1,23 @@
+"""
+=======================
+Load Module from Config
+=======================
+
+In bolero, we can load modules (optimizers, behavior search methods, behaviors,
+environments) either from configuration dictionaries or from YAML files. This
+example shows how we can load an optimizer from a configuration dictionary.
+"""
+print(__doc__)
+from bolero.utils import from_dict
+
+
+config = {
+    "type": "bolero.optimizer.CMAESOptimizer",
+    "variance": 10.0,
+}
+
+optimizer = from_dict(config)
+optimizer.init(2)
+params = [0, 0]
+optimizer.get_next_parameters(params)
+print(params)
