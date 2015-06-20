6ad064a59c37553a5eef18fe145bca7a0e4cd499
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2015-06-21 01:42:53 +0200

    Monkey patch for nosetests

--- bolero/__init__.py
+++ bolero/__init__.py
@@ -2,3 +2,17 @@ __version__ = "0.1"
 
 
 __all__ = ["environment", "optimizer", "representation", "utils", "wrapper"]
+
+
+def setup_module(module):
+    """Monkey patch for nosetests.
+
+    nosetests tries to import all modules and looks for the attribute 'setup'.
+    In our case it finds setup.py and tries to handle it as a function which
+    does not work. So we have to tell nosetests that 'setup' is not a possible
+    name for a setup function.
+    """
+    from nose.suite import ContextSuite
+    moduleSetup = list(ContextSuite.moduleSetup)
+    moduleSetup.remove("setup")
+    ContextSuite.moduleSetup = tuple(moduleSetup)
