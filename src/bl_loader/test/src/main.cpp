3325531ea2dc22da54dd592073cb600519d0cf89
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2016-03-18 15:39:26 +0100

    Fix catch include

--- src/bl_loader/test/src/main.cpp
+++ src/bl_loader/test/src/main.cpp
@@ -1,9 +1,5 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#ifdef ROCK
 #include <catch/catch.hpp>
-#else
-#include "catch.hpp"
-#endif
 #include <LoadableBehavior.h>
 #include <BLLoader.h>
 #include <Optimizer.h>
