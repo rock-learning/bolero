Extract inv_sqrt and fix covariance computation

bolero/optimizer/ccmaes.py
@@ -7,6 +7,7 @@ from ..representation.ul_policies import (ContextTransformationPolicy,
                                           LinearGaussianPolicy)
 from ..utils.validation import check_random_state, check_feedback, check_context
 from ..utils.log import get_logger
+from .cmaes import inv_sqrt
 from sklearn.linear_model import Ridge
 
 
@@ -208,6 +209,9 @@ class CCMAESOptimizer(ContextualOptimizer):
 
             self.weights[indices] = self.ordered_weights
 
+            cov = self.policy_.policy.Sigma
+            invsqrtC = inv_sqrt(cov)
+
             last_W = np.copy(self.policy_.W)
             self.policy_.fit(phi_s, theta, self.weights, context_transform=False)
 
@@ -216,15 +220,6 @@ class CCMAESOptimizer(ContextualOptimizer):
             mean_diff = (self.policy_.W.dot(mean_phi) - last_W.dot(mean_phi)) / sigma
 
             self.ps *= (1.0 - self.c_sigma)
-
-            # TODO refactor?
-            cov = np.copy(self.policy_.policy.Sigma)
-            cov[:, :] = np.triu(cov) + np.triu(cov, 1).T
-            D, B = np.linalg.eigh(cov)
-            # HACK: avoid numerical problems
-            D = np.maximum(D, np.finfo(np.float).eps)
-            D = np.diag(np.sqrt(1.0 / D))
-            invsqrtC = B.dot(D).dot(B.T)
 
             self.ps += (np.sqrt(self.c_sigma * (2.0 - self.c_sigma) *
                                 self.mu_w) * invsqrtC.dot(mean_diff))

bolero/optimizer/cmaes.py
@@ -28,6 +28,16 @@ def _bound(bounds, samples):
             samples[k] = np.minimum(samples[k], bounds[:, 1])
 
 
+def inv_sqrt(cov):
+    """Compute inverse square root of a covariance matrix."""
+    cov = np.triu(cov) + np.triu(cov, 1).T
+    D, B = np.linalg.eigh(cov)
+    # HACK: avoid numerical problems
+    D = np.maximum(D, np.finfo(np.float).eps)
+    D = np.diag(np.sqrt(1.0 / D))
+    return B.dot(D).dot(B.T)
+
+
 class CMAESOptimizer(Optimizer):
     """Covariance Matrix Adaptation Evolution Strategy.
 
@@ -209,7 +219,8 @@ class CMAESOptimizer(Optimizer):
             self.neg_cmu = ((1.0 - self.cmu) * 0.25 * self.mueff /
                             ((self.n_params + 2) ** 1.5 + 2.0 * self.mueff))
 
-        self._update_covariance(self.it)
+        self.invsqrtC = inv_sqrt(self.cov)
+        self.eigen_decomp_updated = self.it
 
     def _sample(self, n_samples):
         samples = self.random_state.multivariate_normal(
@@ -323,18 +334,10 @@ class CMAESOptimizer(Optimizer):
         self.var *= np.exp(np.min((0.6, log_step_size_update))) ** 2
 
         if it - self.eigen_decomp_updated > self.eigen_update_freq:
-            self._update_covariance(it)
+            self.invsqrtC = inv_sqrt(self.cov)
+            self.eigen_decomp_updated = self.it
 
         self.samples = self._sample(self.n_samples_per_update)
-
-    def _update_covariance(self, it):
-        self.eigen_decomp_updated = it
-        self.cov[:, :] = np.triu(self.cov) + np.triu(self.cov, 1).T
-        D, B = np.linalg.eigh(self.cov)
-        # HACK: avoid numerical problems
-        D = np.maximum(D, np.finfo(np.float).eps)
-        D = np.diag(np.sqrt(1.0 / D))
-        self.invsqrtC = B.dot(D).dot(B.T)
 
     def is_behavior_learning_done(self):
         """Check if the optimization is finished.

bolero/optimizer/test/test_contextual.py
@@ -54,6 +54,6 @@ def test_ccmaes():
     x = np.zeros(1)
     opt = CCMAESOptimizer(x, random_state=0)
     opt.init(1, 1)
-    r = eval_loop(x, opt)
-    assert_less(-1e-6, r.max())
+    r = eval_loop(x, opt, n_evals=1000)
+    assert_less(-1e-7, r.max())
     return r