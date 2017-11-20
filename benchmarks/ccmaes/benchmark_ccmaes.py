6fff8c63e5136b0c6d934f529383a0f459ca2a58
Alexander Fabisch <afabisch@informatik.uni-bremen.de>
2017-11-20 21:28:32 +0100

    Unit test for objective function

--- benchmarks/ccmaes/benchmark_ccmaes.py
+++ benchmarks/ccmaes/benchmark_ccmaes.py
@@ -1,26 +1,6 @@
 import numpy as np
 from bolero.optimizer import CCMAESOptimizer, CREPSOptimizer
-from bolero.environment.contextual_objective_functions import \
-    ContextualObjectiveFunction
-from bolero.environment.objective_functions import rosenbrock
-
-
-class Sphere(ContextualObjectiveFunction):
-    def __init__(self, random_state, n_dims, n_context_dims):
-        self.G = random_state.randn(n_dims, n_context_dims)
-
-    def feedback(self, theta, s):
-        x = theta + self.G.dot(s)
-        return -x.dot(x)
-
-
-class Rosenbrock(ContextualObjectiveFunction):
-    def __init__(self, random_state, n_dims, n_context_dims):
-        self.G = random_state.randn(n_dims, n_context_dims)
-
-    def feedback(self, theta, s):
-        x = theta + self.G.dot(s)
-        return -rosenbrock(x)
+from objective import Sphere, Rosenbrock
 
 
 def benchmark():
