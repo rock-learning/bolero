0de0c7453ea9246f612c2f03b259d06203ab036a
Henning Wiedemann <henning.wiedemann@dfki.de>
2018-02-23 07:41:21 +0100

    PSO, CMAES: py-scripts to PEP8, convert to python3

--- src/optimizer/cmaes_optimizer/evalCMAESOutput.py
+++ src/optimizer/cmaes_optimizer/evalCMAESOutput.py
@@ -1,38 +1,39 @@
-#!/bin/python
+#!/bin/python3
 import yaml
 import sys
 import os
 import numpy as np
 
 
-def saw(val, min, max):
-    range = max - min
-    if range == 0.0:
-      return min
+def saw(val, param_min, param_max):
+    param_range = param_max - param_min
+    if param_range == 0.0:
+        return param_min
     else:
-      val = np.mod(np.abs(val), range*2)
-      if(val) > range:
-        val = range*2 - val
-      val += min
-      return val
+        val = np.mod(np.abs(val), param_range * 2)
+        if val > param_range:
+            val = param_range * 2 - val
+        val += param_min
+        return val
 
-if len(sys.argv)>=3:
-  paramf = yaml.load(open(os.path.abspath(sys.argv[2])).read())["Parameters"]
-  cmaesf = open(sys.argv[1])
-  cmaes = cmaesf.read().split('\n')
-  xbeststart = [l[0:9] for l in cmaes].index("xbestever")+1
-  xbestend = [l[0:6] for l in cmaes].index("xbest ")
-  i = xbeststart
-  pOut = []
-  while i<xbestend:
-    pOut= pOut + cmaes[i].split()
-    i+=1
-  if len(paramf)==len(pOut):
-    out = open(os.path.abspath(sys.argv[3]),"w")
-    out.write(cmaes[[l[0:5] for l in cmaes].index("# ---")]+"\n#"+cmaes[xbeststart-1])
-    for i in range(len(pOut)):
-      out.write(paramf[i]["name"]+": "+str(round(saw(float(pOut[i]),paramf[i]["min"],paramf[i]["max"]),5))+"\n")
-  else:
-    print "ERROR: Number of parameters do not match!"
+
+if len(sys.argv) >= 3:
+    param_file = yaml.load(open(os.path.abspath(sys.argv[2])).read())["Parameters"]
+    cmaes = open(sys.argv[1]).read().split('\n')
+    xbeststart = [l[0:9] for l in cmaes].index("xbestever") + 1
+    xbestend = [l[0:6] for l in cmaes].index("xbest ")
+    i = xbeststart
+    pOut = []
+    while i < xbestend:
+        pOut = pOut + cmaes[i].split()
+        i += 1
+    if len(param_file) == len(pOut):
+        out_file = open(os.path.abspath(sys.argv[3]), "w")
+        out_file.write(cmaes[[l[0:5] for l in cmaes].index("# ---")] + "\n#" + cmaes[xbeststart - 1] + "\n")
+        for i in range(len(pOut)):
+            out_file.write(param_file[i]["name"] + ": " + str(
+                round(saw(float(pOut[i]), param_file[i]["min"], param_file[i]["max"]), 5)) + "\n")
+    else:
+        print("ERROR: Number of parameters do not match!")
 else:
-  print "Failed. Usage: python evalCMAESOutput.py $allcmaes.dat $param.yml $output_parameters.yml"
+    print("Failed. Usage: python evalCMAESOutput.py $allcmaes.dat $param.yml $output_parameters.yml")
--- src/optimizer/pso_optimizer/evalPSOOutput.py
+++ src/optimizer/pso_optimizer/evalPSOOutput.py
@@ -5,27 +5,29 @@ import os
 import numpy as np
 
 
-def saw(val, min, max):
-    range = max - min
-    if range == 0.0:
-      return min
+def saw(val, param_min, param_max):
+    param_range = param_max - param_min
+    if param_range == 0.0:
+        return param_min
     else:
-      val = np.mod(np.abs(val), range*2)
-      if(val) > range:
-        val = range*2 - val
-      val += min
-      return val
+        val = np.mod(np.abs(val), param_range * 2)
+        if val > param_range:
+            val = param_range * 2 - val
+        val += param_min
+        return val
 
-if len(sys.argv)>=3:
-  paramf = yaml.load(open(os.path.abspath(sys.argv[2])).read())["Parameters"]
-  psof = open(sys.argv[1]).read().split("\n")
-  pOut = psof[2].split(', ')
-  if len(paramf)==(len(pOut)-1):
-    out = open(os.path.abspath(sys.argv[3]),"w")
-    out.write("#"+psof[0])
-    for i in range(len(pOut)-1):
-      out.write(paramf[i]["name"]+": "+str(round(saw(float(pOut[i]),paramf[i]["min"],paramf[i]["max"]),5))+"\n")
-  else:
-    print "ERROR: Number of parameters do not match!"
+
+if len(sys.argv) >= 3:
+    param_file = yaml.load(open(os.path.abspath(sys.argv[2])).read())["Parameters"]
+    pso_file = open(sys.argv[1]).read().split("\n")
+    pOut = pso_file[2].split(', ')
+    if len(param_file) == (len(pOut) - 1):
+        out_file = open(os.path.abspath(sys.argv[3]), "w")
+        out_file.write("#" + pso_file[0] + "\n")
+        for i in range(len(pOut) - 1):
+            out_file.write(param_file[i]["name"] + ": " + str(
+                round(saw(float(pOut[i]), param_file[i]["min"], param_file[i]["max"]), 5)) + "\n")
+    else:
+        print "ERROR: Number of parameters do not match!"
 else:
-  print "Failed. Usage: python evalPSOOutput.py $pso_best_params.dat $param.yml $output_parameters.yml"
+    print "Failed. Usage: python evalPSOOutput.py $pso_best_params.dat $param.yml $output_parameters.yml"
