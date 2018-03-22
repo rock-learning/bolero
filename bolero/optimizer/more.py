8787dd73885c60301582688e0dcceca37c06361d
Hans Hohenfeld <hans.hohenfeld@dfki.de>
2018-03-22 16:25:32 +0100

    MORE: Optimizer implementation and BLR Surrogate model for objective function / WIP

--- bolero/optimizer/more.py
+++ bolero/optimizer/more.py
@@ -2,13 +2,40 @@
 
 """ MOdel based Relative Entropy stochastic search """
 
+from collections import deque
+
 import numpy as np
 from scipy.optimize import minimize
+from scipy.stats import multivariate_normal
 
 from .optimizer import Optimizer
 
 from ..utils.log import get_logger
-from ..utils import check_random_state
+from ..utils.validation import check_feedback, check_random_state
+
+class QuadraticSurrogateModel(object):
+    """ A Quadratic surrogate model for the objective function.
+
+    The quadratic surrogate model is a local quadratic approximation of the value function
+    learned from previous data. The data is fitted by a Bayesian linear regression model.
+    """
+
+    def __init__(self):
+        """ The constructor """
+        pass
+
+    def fit(self, X, y):
+        """ Fit Bayesian Linear Regression model.
+
+        Parameters
+        ----------
+        X: array, shape (n_dims, n_samples)
+	    Input data
+        y: array, shape (n_samples,)
+	    Target values
+
+        """
+        pass
 
 
 class MOREOptimizer(Optimizer):
@@ -31,8 +58,8 @@ class MOREOptimizer(Optimizer):
         in Neural Information Processing Systems. 2015. S. 3537-3545.
     """
     def __init__(self, initial_params=None, bounds=None,
-                 # TODO: Check those defaults
-                 epsilon=1.5, beta=-1000,
+                 epsilon=3.0, beta=-5000.0, train_freq=25,
+                 n_samples_per_update=100,
                  log_to_stdout=False, log_to_file=False, random_state=None):
         self.initial_params = initial_params
         self.bounds = bounds
@@ -47,8 +74,22 @@ class MOREOptimizer(Optimizer):
 
         self.iteration = 0
 
+        self.n_samples_per_update = n_samples_per_update
+        self.train_freq = train_freq
+
         self.logger = None
         self.n_params = None
+
+        self.params = None
+        self.reward = None
+
+        self.max_reward = -np.inf
+        self.best_params = None
+
+        self.reward_model = QuadraticSurrogateModel()
+
+        self.hist_params = deque(maxlen=self.n_samples_per_update)
+        self.hist_rewards = deque(maxlen=self.n_samples_per_update)
 
     def init(self, n_params):
         """Initialize the behavior search.
@@ -74,6 +115,8 @@ class MOREOptimizer(Optimizer):
                 raise ValueError("Initial parameter vector has wrong dimension. Is %d, expected %d!"
                                  % (len(self.initial_params), self.n_params))
 
+        self.best_params = self.initial_params.copy()
+
     def get_next_parameters(self, params):
         """Get next individual/parameter vector for evaluation.
 
@@ -82,6 +125,8 @@ class MOREOptimizer(Optimizer):
         params : array_like, shape (n_params,)
             Parameter vector, will be modified
         """
+        self._sample_from_policy()
+        params[:] = self.params
 
     def set_evaluation_feedback(self, rewards):
         """Set feedbacks for the parameter vector.
@@ -91,6 +136,25 @@ class MOREOptimizer(Optimizer):
         rewards : list of float
             feedbacks for each step or for the episode, depends on the problem
         """
+        self.reward = check_feedback(rewards, compute_sum=True)
+
+        self.hist_params.append(self.params)
+        self.hist_rewards.append(self.reward)
+
+        self.iteration += 1
+
+        # TODO: Surrogate Model / Policy update
+        if self.iteration % self.train_freq == 0:
+            X = np.asarray(self.hist_params)
+            y = np.asarray(self.hist_rewards)
+
+            self.reward_model.fit(X=X, y=y)
+
+            self._update_policy()
+
+        if self.reward > self.max_reward:
+            self.max_reward = self.reward
+            self.best_params = self.params
 
     def is_behavior_learning_done(self):
         """Check if the optimization is finished.
@@ -100,6 +164,7 @@ class MOREOptimizer(Optimizer):
         finished : bool
             Is the learning of a behavior finished?
         """
+        return False
 
     def get_best_parameters(self):
         """Get best individual/parameter vector so far.
@@ -109,6 +174,15 @@ class MOREOptimizer(Optimizer):
         p : array_like, shape (n_params,)
             Best parameter vector so far
         """
+        return self.best_params
+
+    def _sample_from_policy(self):
+        """ Sample new parameter vector from current search distribution. """
+        pass
+
+    def _update_policy(self):
+        """ Update the search distribution """
+        pass
 
     def _solve_dual_problem(self):
         """ Solve the dual optimization problem """
