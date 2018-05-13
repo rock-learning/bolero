99218f02ce11be2dd362a139f756993baf3df8e1
Bernd Poppinga <bernd.poppinga@dfki.de>
2018-05-13 20:58:54 +0200

    added ProMPs

---
+++ src/representation/promp/implementation/src/BasisFunctions.h
@@ -0,0 +1,59 @@
+#pragma once 
+
+#include <Eigen/Dense>
+namespace promp{
+using namespace Eigen;
+
+
+  
+class BasisFunctions{
+public:
+  MatrixXd getValue(const VectorXd& time, int dimensions = 1) const;
+  MatrixXd getValue(const double time, int dimensions = 1) const;
+  
+  MatrixXd getValueDeriv(const VectorXd& time, int dimensions = 1) const;
+  MatrixXd getValueDeriv(const double time, int dimensions = 1) const;
+  
+  MatrixXd getValueAndDeriv(const VectorXd& time, int dimensions = 1) const;
+  MatrixXd getValueAndDeriv(const double time, int dimensions = 1) const;
+
+protected:
+  BasisFunctions(const int numBF,const double overlap = 0.5);
+  
+  virtual VectorXd calcBasisFunction(const VectorXd& z, const int i) const  = 0 ;
+  virtual VectorXd calcBasisFunctionDeriv(const VectorXd& z, const int i) const  = 0;
+
+  VectorXd calcNormalizedBasisFunction(const VectorXd& z, const int i) const;
+  VectorXd calcNormalizedBasisFunctionDeriv(const VectorXd& z, const int i) const;
+  
+  double getMean(int bf) const; 
+  
+  int numBF_;
+  double h_;      
+  
+};
+
+class StrokeBasisFunctions : public BasisFunctions{
+public:
+  StrokeBasisFunctions(const int numBF, const double overlap = 0.5) : BasisFunctions(numBF,overlap){};
+private:
+  VectorXd calcBasisFunction(const VectorXd& z, const int i) const;
+  VectorXd calcBasisFunctionDeriv(const VectorXd& z, const int i) const;
+};
+
+class PeriodicBasisFunctions : public BasisFunctions{
+public:
+  PeriodicBasisFunctions(const int numBF, const double overlap = 0.5) : BasisFunctions(numBF,overlap){};
+private:
+  VectorXd calcBasisFunction(const VectorXd& z, const int i) const;
+  VectorXd calcBasisFunctionDeriv(const VectorXd& z, const int i) const;
+};
+
+
+
+
+
+
+
+
+};
---
+++ src/representation/promp/implementation/src/Trajectory.h
@@ -0,0 +1,124 @@
+#pragma once
+
+#include <Eigen/Dense>
+#include "BasisFunctions.h"
+#include <vector>
+#include "promp.h"
+#include <chrono>
+#include <memory>
+
+namespace promp {
+  using namespace Eigen;
+
+  enum TrajectoryType {
+    Stroke,
+    Periodic
+  };
+
+  struct ConditionPoint {
+    double timestamp;
+    int dimension;
+    int derivative;
+    double mean;
+    double variance;
+
+    static std::vector<ConditionPoint> fromMatrix(MatrixXd pointMatrix);
+    const static unsigned NUM_FIELDS = 5;
+  };
+
+  class TrajectoryBase {
+  public:
+    virtual MatrixXd getValueMean(const VectorXd &time) const = 0;
+
+    virtual VectorXd getValueMean(const double time) const = 0;
+
+    virtual std::vector<MatrixXd> getValueCovars(const VectorXd &time) const =0;
+
+    virtual MatrixXd getValueCovars(const double time) const =0;
+  };
+
+
+  class Trajectory{
+  public:
+    Trajectory(const TrajectoryData& data);
+
+    Trajectory(const int numWeights, const VectorXd &weights, const double overlap, const MatrixXd &covars = MatrixXd::Zero(0, 0),
+               const TrajectoryType type = Stroke);
+
+    Trajectory(const std::vector<VectorXd> &timestamps, const std::vector<MatrixXd> &values, const double overlap, int numWeights = 30,
+               const TrajectoryType type = Stroke);
+
+    void imitate(const std::vector<VectorXd> &timestamps, const std::vector<MatrixXd> &values);
+
+    MatrixXd getWeightMean() const;
+
+    MatrixXd getWeightCovars() const;
+
+    MatrixXd getValueMean(const VectorXd &time) const;
+
+    VectorXd getValueMean(const double time) const;
+
+    MatrixXd getValueCovars(const VectorXd &time) const;
+
+    VectorXd getValueCovars(const double time) const;
+
+    void setConditions(const std::vector<ConditionPoint>& conditionPoints){
+      conditionPoints_ = conditionPoints;
+    };
+
+    void getData(TrajectoryData &data) const;
+
+    Trajectory sampleTrajectoty() const;
+
+    const int numWeights_;
+    const int numDim_;
+    const double overlap_;
+    static constexpr int numFunc_ = 2; // position and acceleration
+    void beginTimeMeasure() const;
+
+    double endTimeMeasure() const;
+  private:
+    //TrajectoryData data;
+    void E_Step(const MatrixXd &mean_eStep, const MatrixXd &cov_eStep, Ref<VectorXd, 0, InnerStride<>> mean, MatrixXd &cov);
+
+    void M_Step(const MatrixXd &mean, const std::vector<MatrixXd> &cov);
+
+    void M_Step2(const MatrixXd &mean, const std::vector<MatrixXd> &cov, const std::vector<MatrixXd> &RR,
+                 const std::vector<MatrixXd> &RH, const std::vector<MatrixXd> &HH, const int sampleCount);
+
+    void setBF();
+
+    void condition(VectorXd weightMean,MatrixXd weightCovars) const;
+
+    VectorXd weightMean_;
+    MatrixXd weightCovars_;
+    
+    std::vector<ConditionPoint> conditionPoints_;
+    double standardDev_;
+    TrajectoryType type_;
+    std::shared_ptr<BasisFunctions> basisFunctions_ = nullptr;
+    mutable std::chrono::high_resolution_clock::time_point start;
+  };
+
+  class CombinedTrajectory{
+  public:
+    CombinedTrajectory(const CombinedTrajectoryData &data);
+
+    CombinedTrajectory(const std::vector<Trajectory> &trajectories, const std::vector<MatrixXd> &activations);
+
+    MatrixXd getValueMean(const VectorXd &time) const;
+
+    VectorXd getValueMean(const double time) const;
+
+    std::vector<MatrixXd> getValueCovars(const VectorXd &time) const;
+
+    MatrixXd getValueCovars(const double time) const;
+
+  private:
+    std::vector<Trajectory> trajectories_;
+    std::vector<MatrixXd> activations_;
+
+    double getActivation(const double time, const int index) const;
+  };
+
+}
